4c2f23638a73e8a873457342b450712f257fa718
rafidashab <rafidashabpranta@gmail.com>
2019-03-03 20:57:45 -0800

    Fix second hand slowdown bug

--- time.js
+++ time.js
@@ -1,11 +1,14 @@
 function initLocalClocks() {
 
-  var today = new Date(); //fucking date
+  var today = new Date(); // Creates a date object
+
+  //store the current hours
   var hours = today.getHours();
   var minutes = today.getMinutes();
   var seconds = today.getSeconds();
 
-  var hour_angle = (hours * 30) + (minutes / 2);
+  
+  var hour_angle = (hours * 30) + (minutes / 2); // 6:59 would translate to 180+ (30) = 210 which is almost equal to 7
   var minutes_angle = minutes * 6;
   var seconds_angle = seconds * 6;
 
@@ -18,8 +21,9 @@ function initLocalClocks() {
   var x = document.querySelectorAll(".seconds-container");
   x[0].style.transform =  "rotateZ(" + seconds_angle + "deg)";
 
+  requestAnimationFrame(initLocalClocks);
 }
 
-function test() {
-  document.body.style.backgroundColor = "Black"
-}
+// function test() {
+//   document.body.style.backgroundColor = "Black"
+// }
